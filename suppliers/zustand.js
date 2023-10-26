38ee143396fcb1d4c3111e803ae365d2c4631135
Siddhant <94226898+S1D007@users.noreply.github.com>
2023-10-26 09:32:50 +0530

    Update zustand.js

--- suppliers/zustand.js
+++ suppliers/zustand.js
@@ -3,7 +3,7 @@ import { create } from "zustand";
 import axios from "axios";
 import { toast } from "react-toastify";
 
-const BACKEND_URL = "https://3.109.181.36.nip.io";
+const BACKEND_URL = "https://3.110.128.27.nip.io";
 
 const api = axios.create({
     baseURL: BACKEND_URL,
